0c03ac1af878d19a23062efcf1add392d8851fb4
sga-arun <arun.taneja@sgainc.com>
2020-06-30 22:29:07 +0530

    initial commit

---
+++ src/main/java/com/example/demo/model/Book.java
@@ -0,0 +1,67 @@
+package com.example.demo.model;
+
+import org.apache.solr.client.solrj.beans.Field;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.solr.core.mapping.Indexed;
+import org.springframework.data.solr.core.mapping.SolrDocument;
+
+@SolrDocument(collection = "demo")
+public class Book {
+
+	@Id
+	@Field
+	private String id;
+
+	@Field
+	private String name;
+
+	@Field
+	private Boolean inStock;
+
+	@Indexed(name = "genre_s")
+	private String genre;
+	
+	public Book() {
+		
+	}
+
+	public Book(String id, String name, Boolean inStock, String genre) {
+		this.id = id;
+		this.name = name;
+		this.inStock = inStock;
+		this.genre = genre;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Boolean getInStock() {
+		return inStock;
+	}
+
+	public void setInStock(Boolean inStock) {
+		this.inStock = inStock;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+}
---
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -0,0 +1,20 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.solr.core.query.result.HighlightPage;
+import org.springframework.data.solr.repository.Highlight;
+import org.springframework.data.solr.repository.Query;
+import org.springframework.data.solr.repository.SolrCrudRepository;
+
+import com.example.demo.model.Book;
+
+public interface BookRepository extends SolrCrudRepository<Book, String> {
+
+	@Query("name:*?0* OR genre_t:*?0*")
+	public List<Book> findAll(String query);
+
+	@Highlight(prefix = "<strong>", postfix = "</strong>", fields = { "name" })
+	HighlightPage<Book> findByNameLike(String name, Pageable page);
+}
